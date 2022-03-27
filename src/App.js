986fee7e230205126898f3760599d943612ddcb0
marcoswi <marcoswithington@gmail.com>
2022-03-27 21:39:37 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Curso ReactJS - Withington
+          Curso ReactJS - DMRSoaps - Withington
         </p>
         <a
           className="App-link"
