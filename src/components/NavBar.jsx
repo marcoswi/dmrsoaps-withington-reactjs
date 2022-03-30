330ba1d548dfbaed0e114e0e3f61e725837c8c13
marcoswi <marcoswithington@gmail.com>
2022-03-30 18:27:46 +0200

    navbar basico

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <>
+        <header class="">
+            <nav class="">
+                <a href="/#"><img src="" alt="Logo DMR Soaps" /></a> 
+                <div>
+                    <ul>
+                        <li>
+                            <a href="/#">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="/#">Productos</a>
+                        </li>
+                        <li>
+                            <a href="/#">Ofertas</a>
+                        </li>
+                        <li>
+                            <a href="/#">Sobre Nosotros</a>
+                        </li>
+                        <li>
+                            <a href="/#">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        </>
+    );
+}
